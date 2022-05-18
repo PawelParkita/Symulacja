83ad48928b4e8690300028858445dece40f60060
malinovskovy <74260398+malinovskovy@users.noreply.github.com>
2022-05-18 22:28:02 +0200

    Update Main.java

--- Symulacja/src/Main.java
+++ Symulacja/src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
-
-
-
+    int alien_population, human_population, probability, mountains, lakes;
 
     public static void main(String[] args) {
+        Simulation simulation = new Simulation();
+        simulation.start_simulation();
     }
-
 
 }
