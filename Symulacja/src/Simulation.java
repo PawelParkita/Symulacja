21b25c33927b30f6f937d4d91fb82f8a22570d6b
malinovskovy <74260398+malinovskovy@users.noreply.github.com>
2022-05-24 20:48:41 +0200

    Update Simulation.java

--- Symulacja/src/Simulation.java
+++ Symulacja/src/Simulation.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Simulation {
 
     void movement(){
@@ -10,10 +12,15 @@ public class Simulation {
     }
 
     void start_simulation(){
-        Map map = new Map(25, 25, 5, 5,10,10, 50);
+        Map map = new Map(10, 10, 2, 2,5,5, 50);
         map.map_initialization();
         map.map_population();
-        map.print_map();
+
+        for(int i=0; i<3; i++){
+            map.print_map();
+            System.out.println("");
+            map.movement();
+        }
     }
 
 }
