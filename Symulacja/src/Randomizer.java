5072d533e13649dfcab3e5b7e9dd00c2ffcf01d6
malinovskovy <74260398+malinovskovy@users.noreply.github.com>
2022-05-24 20:49:04 +0200

    Create Randomizer.java

---
+++ Symulacja/src/Randomizer.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Randomizer
+{
+    char random_sex(){
+        Random rand=new Random();
+        int s = rand.nextInt(2);
+        if (s==0)  return 'M';
+        else return 'W';
+
+    }
+
+    int random_power(){
+    Random rand=new Random();
+        return rand.nextInt(100);
+}
+
+    int random_speed(){
+        Random rand=new Random();
+       return rand.nextInt(3)+1;
+    }
+}
