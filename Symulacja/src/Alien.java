43eb5f74ecdbe559469a4b1d597197024109e399
malinovskovy <74260398+malinovskovy@users.noreply.github.com>
2022-05-24 20:45:47 +0200

    Update Alien.java

--- Symulacja/src/Alien.java
+++ Symulacja/src/Alien.java
@@ -4,12 +4,12 @@ public class Alien extends Agent {
 
     int power,speed;
 
+    public Alien( int x, int y,int power,int speed){
 
-    public Alien( int x, int y) {
-        super (x,y);
-        Random rand=new Random();
-        this.power=rand.nextInt(100);
-        this.speed= rand.nextInt(3)+1;
+        super (x,y,'A',power,speed);
+        //Random rand=new Random();
+        //this.power=rand.nextInt(100);
+        //this.speed= rand.nextInt(3)+1;
 
 
 
