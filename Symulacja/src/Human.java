ba6690f787233cbd4a7c07c697c4b7bf56551bcb
malinovskovy <74260398+malinovskovy@users.noreply.github.com>
2022-05-24 20:47:38 +0200

    Update Human.java

--- Symulacja/src/Human.java
+++ Symulacja/src/Human.java
@@ -2,19 +2,26 @@ import java.util.Random;
 
 public class Human extends Agent{
 
-    char sex;
+   // char sex;
 
-    public Human(int x, int y){
-        super(x,y);
-        Random rand=new Random();
-        int s = rand.nextInt(2);
-        if (s==0)  this.sex='M';
-        else this.sex='W';
+    //char S(){
+    //Random rand=new Random();
+    //int s = rand.nextInt(2);
+    //if (s==0)  this.sex='M';
+    //else this.sex='W';
+    //return sex;
+    //}
+
+    public Human(int x, int y,char sex){
+        super(x,y,sex,0,1);
+        //Random rand=new Random();
+        //int s = rand.nextInt(2);
+        //if (s==0)  this.sex='M';
+        //else this.sex='W';
      }
 
 
-
-
-
-
+    public Human(int x, int y, char sex, int power) {
+        super(x,y,sex,power,1);
+    }
 }
