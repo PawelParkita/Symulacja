1525d0d443b0733855298635fd45599f17c16730
malinovskovy <74260398+malinovskovy@users.noreply.github.com>
2022-05-24 20:46:28 +0200

    Update Guardian.java

--- Symulacja/src/Guardian.java
+++ Symulacja/src/Guardian.java
@@ -2,11 +2,11 @@ import java.util.Random;
 
 public class Guardian extends Human {
 
-    int power;
+   //int power;
 
-    public Guardian( int x, int y) {
-        super(x,y);
-        Random rand=new Random();
-        this.power=rand.nextInt(100);
+    public Guardian( int x, int y, char sex, int power) {
+        super(x,y,sex,power);
+        //Random rand=new Random();
+        //this.power=rand.nextInt(100);
     }
 }
