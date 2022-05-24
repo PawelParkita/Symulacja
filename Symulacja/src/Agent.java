e8ea5edfb9e512311315a82494eb7c58b65fb6a2
malinovskovy <74260398+malinovskovy@users.noreply.github.com>
2022-05-24 20:44:50 +0200

    Update Agent.java

--- Symulacja/src/Agent.java
+++ Symulacja/src/Agent.java
@@ -1,10 +1,13 @@
 abstract class Agent {
 
     int x,y;
-
-     public Agent (int x, int y){
+    char sex;
+    int power,speed;
+     public Agent (int x, int y, char sex,int power,int speed){
          this.x=x;
          this.y=y;
-
+         this.sex=sex;
+         this.power=power;
+         this.speed=speed;
      }
 }
