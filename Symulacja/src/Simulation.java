8013317afe944f789f416626d18bc3a58b53df6d
PawelParkita <105748525+PawelParkita@users.noreply.github.com>
2022-06-12 23:07:51 +0200

    limits

--- Symulacja/src/Simulation.java
+++ Symulacja/src/Simulation.java
@@ -7,7 +7,7 @@ public class Simulation {
         Data data = new Data();
       //  tab=data.Data();
       //  Map map = new Map(tab[0], tab[1], tab[2], tab[3], tab[5], tab[4], tab[6]);
-        Map map = new Map(25, 25,4, 3, 20,20,70);
+        Map map = new Map(15, 15,4, 3, 20,20,70);
         map.map_initialization();
         map.map_population();
 
@@ -22,8 +22,8 @@ public class Simulation {
 
     boolean stop_simulation(Map map) {
 
-       if (map.human_population <= 0) return false;
-       if (map.alien_population <=0) return false;
+       if (map.human_population <= 0  || (map.width>10 && map.human_population < (0.1*map.alien_population)) ) return false;
+       if (map.alien_population <=0 || (map.width>10 && map.alien_population < (0.1*map.human_population))) return false;
        if (map.loops_witout_action>=20000) return false;
 
     return true;
